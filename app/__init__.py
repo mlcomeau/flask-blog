cfa3a3730e83908b70d0e171c4700b92d717f3ce
Megan Comeau <mlcomeau99@gmail.com>
2021-06-22 15:40:36 -0700

    adds login and registration endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ import flask
 from dotenv import load_dotenv
 from flask_mail import Mail, Message
 from . import db 
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.db import get_db
 
 load_dotenv()
 app = Flask(__name__)
@@ -47,6 +49,63 @@ def index():
 def health():
     status_code = flask.Response(status=200)
     return status_code
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return a restister page
+    return "Register Page not yet implemented", 501
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
+    
+    ## TODO: Return a login page
+    return "Login Page not yet implemented", 501
+
+
 
 # if __name__ == '__main__':
 #     app.run(host='127.0.0.1', port=5000, debug=False)
