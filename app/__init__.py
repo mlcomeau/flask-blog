4003941fd541e972cac7c10504147917b1a9a6db
Megan Comeau <mlcomeau99@gmail.com>
2021-07-01 10:23:32 -0700

    switch to postgres

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,41 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
-import flask
 from dotenv import load_dotenv
 from flask_mail import Mail, Message
-from . import db 
 from werkzeug.security import generate_password_hash, check_password_hash
-from app.db import get_db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 load_dotenv()
 app = Flask(__name__)
 
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-db.init_app(app)
+## Data base configuration 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
+    user=os.getenv('POSTGRES_USER'),
+    passwd=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST'),
+    port=5432,
+    table=os.getenv('POSTGRES_DB'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
 
 # Configuration for flask_mail 
 # This setup is specifically for gmail, other email servers have different configuration settings 
@@ -55,24 +79,19 @@ def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return redirect(url_for('index'))
         else:
             return render_template('register.html', error=error)
@@ -83,19 +102,15 @@ def register():
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
-    error = None
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
-        
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        error = None
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user.password, password):
             error = 'Incorrect password.'
 
         if error is None:
@@ -103,11 +118,6 @@ def login():
         else:
             return render_template('login.html', error=error)
     
-    return render_template('login.html', error=error)
-
-
-
-# if __name__ == '__main__':
-#     app.run(host='127.0.0.1', port=5000, debug=False)
+    return render_template('login.html')
 
 
