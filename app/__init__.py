c62e3522eebc90a3845868bb33949e26a2c494e9
Megan Comeau <mlcomeau99@gmail.com>
2021-07-07 10:47:00 -0700

    add workflows

--- app/__init__.py
+++ app/__init__.py
@@ -9,22 +9,26 @@ from flask_migrate import Migrate
 load_dotenv()
 app = Flask(__name__)
 
-## Data base configuration 
+## Data base configuration
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
-    user=os.getenv('POSTGRES_USER'),
-    passwd=os.getenv('POSTGRES_PASSWORD'),
-    host=os.getenv('POSTGRES_HOST'),
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
     port=5432,
-    table=os.getenv('POSTGRES_DB'))
+    table=os.getenv("POSTGRES_DB"),
+)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username = db.Column(db.String(), primary_key=True)
     password = db.Column(db.String())
@@ -37,53 +41,61 @@ class UserModel(db.Model):
         return f"<User {self.username}>"
 
 
-# Configuration for flask_mail 
-# This setup is specifically for gmail, other email servers have different configuration settings 
-app.config['MAIL_SERVER'] = "smtp.gmail.com"
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-# These need to be setup in .env file 
-app.config['MAIL_USERNAME'] = os.getenv("EMAIL")
-app.config['MAIL_PASSWORD'] = os.getenv("EMAIL_PASSWORD")
+# Configuration for flask_mail
+# This setup is specifically for gmail, other email servers have different configuration settings
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+# These need to be setup in .env file
+app.config["MAIL_USERNAME"] = os.getenv("EMAIL")
+app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_PASSWORD")
 
-# Emails are managed through a mail instance 
+# Emails are managed through a mail instance
 mail = Mail(app)
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # Get form data 
-        name = request.form.get('name')
-        email = request.form.get('email')
-        message = request.form.get('message')
+        # Get form data
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
 
-        # Compose email and send 
-        msg = Message(subject=f"Mail from {name}", body=f"Name: {name}\nEmail: {email}\n\nMessage: {message}", recipients=[os.getenv("EMAIL")], sender=os.getenv("EMAIL"))
+        # Compose email and send
+        msg = Message(
+            subject=f"Mail from {name}",
+            body=f"Name: {name}\nEmail: {email}\n\nMessage: {message}",
+            recipients=[os.getenv("EMAIL")],
+            sender=os.getenv("EMAIL"),
+        )
         mail.send(msg)
 
-        # success value determines that success alert will appear 
-        return render_template('index.html', success=True , title="MLH Fellow", url=os.getenv("URL"))
+        # success value determines that success alert will appear
+        return render_template(
+            "index.html", success=True, title="MLH Fellow", url=os.getenv("URL")
+        )
+
+    return render_template("index.html", title="MLH Fellow", url=os.getenv("URL"))
 
 
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
-
-@app.route('/health')
+@app.route("/health")
 def health():
     return "Its working!"
 
-@app.route('/register', methods=('GET', 'POST'))
+
+@app.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
@@ -91,32 +103,30 @@ def register():
             new_user = UserModel(username, generate_password_hash(password))
             db.session.add(new_user)
             db.session.commit()
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
         else:
-            return render_template('register.html', error=error)
+            return render_template("register.html", error=error)
 
     ## TODO: Return a restister page
-    return render_template('register.html')
+    return render_template("register.html")
 
 
-@app.route('/login', methods=('GET', 'POST'))
+@app.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
         user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
-            error = 'Incorrect username.'
+            error = "Incorrect username."
         elif not check_password_hash(user.password, password):
-            error = 'Incorrect password.'
+            error = "Incorrect password."
 
         if error is None:
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
         else:
-            return render_template('login.html', error=error)
-    
-    return render_template('login.html')
+            return render_template("login.html", error=error)
 
-
+    return render_template("login.html")
